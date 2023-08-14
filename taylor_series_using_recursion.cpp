c5e31d1f4ca7f4b6a4c1c4f265778fb8cb4448f5
kabirsingh201 <125553934+kabirsingh201@users.noreply.github.com>
2023-08-14 22:32:22 +0530

    Add files via upload

---
+++ taylor_series_using_recursion.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+double e(int x, int n){
+    static double p=1,f=1;
+    double r;
+    if(n==0){
+        return 1;
+    }
+    else{
+        r=(x,n-1);
+        p=p*x;
+        f=f*n;
+        return r+p/f;
+    }
+}
+int main() {
+   int x,n,a;
+   cin>>x>>n;
+   a=e(x,n);
+   cout<<a;
+    return 0;
+}
