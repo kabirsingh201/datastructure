901240788de0736c5a16e8b9ca30c39c84c06957
kabirsingh201 <125553934+kabirsingh201@users.noreply.github.com>
2023-08-17 21:25:40 +0530

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+int A[3][4]={{1,2,3,4},{2,4,6,8},{1,3,5,7}};
+int *B[3];  
+int **C;
+int i,j;
+
+for(i=0;i<3;i++)
+ {
+ for(j=0;j<4;j++)
+ cout<<A[i][j]<<endl;
+ 
+ }
+
+B[0]=new int[4];
+B[1]=new int[4];
+B[2]=new int[4];
+for(i=0;i<3;i++)
+ {
+ for(j=0;j<4;j++)
+ cout<<B[i][j]<<endl;
+ 
+ }
+C= new int*[3];
+C[0]=new int[4];
+C[1]=new int[4];
+C[2]=new int[4];
+
+for(i=0;i<3;i++)
+ {
+ for(j=0;j<4;j++)
+ cout<<C[i][j]<<endl;
+ 
+ }
+
+
+    return 0;
+}
